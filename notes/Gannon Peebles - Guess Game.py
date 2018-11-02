8b336745c1f023663288d2a7b82fb9309a2b496b
gannonp <42385579+gannonp@users.noreply.github.com>
2018-11-02 09:44:45 -0700

    Fixed Loops Partially
    Changed from for loop to while loop, still needs to be fixed. November 2nd

--- notes/Gannon Peebles - Guess Game.py
+++ notes/Gannon Peebles - Guess Game.py
@@ -1,11 +1,6 @@
 import random
-number = (random.randint(0,10))
+number = random.randint(1,1)
 
-guess1 = int(input("Guess a number between 1 and 10. "))
-def number(guess):
-    if guess1 == number:
-        return "Correct"
-    if not guess1 == number:
-        return "Incorrect"
-
-
+guess = int(input("Guess a number between 1 and 10. "))
+while number == guess:
+    print("Correct")
