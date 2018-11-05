65d6ed83fd6f80b0634db4080e10db0065cd90d3
gannonp <42385579+gannonp@users.noreply.github.com>
2018-11-05 09:11:08 -0800

    Completed
    It works fully and completely, November 5, 2018.

--- notes/Gannon Peebles - Guess Game.py
+++ notes/Gannon Peebles - Guess Game.py
@@ -1,6 +1,22 @@
 import random
-number = random.randint(1,1)
+attempts = 1
+number = random.randint(1,10)
+isCorrect = False
 
 guess = int(input("Guess a number between 1 and 10. "))
-while number == guess:
-    print("Correct")
+
+while number != guess and attempts < 5:
+    if guess < number:
+        print("Higher")
+    elif guess > number:
+        print("Lower")
+    guess = int(input("Take a guess."))
+    attempts += 1
+
+if attempts == 5:
+    print("You ran out of tries")
+    print("The correct number was %d" % number)
+
+else:
+    print("You got it right!")
+    print("You got it right in %d attempts" % attempts)
