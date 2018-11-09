11adf6f0cb48f33e6397a90c764c6c2dc5e0e4a8
gannonp <42385579+gannonp@users.noreply.github.com>
2018-11-09 10:36:30 -0800

    Added New Feature
    The program now displays the amount of rounds played and keeps the most amount of money you had at one time. November 7th, 2018

--- notes/Lucky 7's.py
+++ notes/Lucky 7's.py
@@ -1,5 +1,7 @@
 import random
 money = 15
+rounds = 0
+most = 15
 
 while money >= 1 and money < 30:
     dice1 = random.randint(1, 6)
@@ -10,6 +12,8 @@ while money >= 1 and money < 30:
         print("You got 7!")
         print(money)
         print()
+        rounds += 1
+
     if dice1 + dice2 > 7 or dice1 + dice2 < 7:
         print("You did not get seven")
         money -= 1
@@ -17,3 +21,13 @@ while money >= 1 and money < 30:
         print("You rolled a")
         print(dice1 and dice2)
         print()
+        rounds += 1
+    if money > most:
+        most = money
+
+print("You played")
+print(rounds)
+print("rounds")
+print()
+print("Your most amount of money was")
+print(most)
