6cf255f911c310808cbe62bac828c452a0113bea
gannonp <42385579+gannonp@users.noreply.github.com>
2018-11-28 09:44:41 -0800

    Mad Lib Part 2 Finished
    November 28, 2018

---
+++ notes/Gannon Peebles - Mad Lib Part 2.py
@@ -0,0 +1,25 @@
+list = []
+list.append(input("Name a noun?"))
+list.append(input("Another adjective, example: good."))
+list.append(input("A person or animal."))
+list.append(input("What are you protesting?"))
+list.append(input("What is one of your possessions?"))
+list.append(input("Another adjective please"))
+list.append(input("Name an action"))
+list.append(input("Another adjective"))
+list.append(input("Name a noun"))
+list.append(input("Name an animal"))
+list.append(input("Name a place"))
+
+
+print("There are many people who think that %s is %s, when in reality, it is not." %
+      (list[0], list[1]))
+print("These %s sometimes believe strongly enough that they protest against laws trying to stop %s."
+      % (list[2], list[3]))
+print("Even though we have the %s of %s, the government still does not listen to those protesters and"
+      % (list[4], list[5]))
+print("continue on with the plan they had. Other than calling the state and asking them whether or"
+      " not they will %s" % list[6])
+print("%s on a %s, there isn’t much you can do to keep the protesters from getting their way,"
+      " but with every" % (list[7], list[8]))
+print("vote against it, more and more %s keep coming from %s." % (list[9], list[10]))
