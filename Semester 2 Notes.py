36d35041ee232165ce11daa0cfd1121a5ed9f1cc
gannonp <42385579+gannonp@users.noreply.github.com>
2019-01-08 09:44:34 -0800

    Semester 2 Notes

---
+++ Semester 2 Notes.py
@@ -0,0 +1,29 @@
+print("Hello world")
+
+# g ball yt
+
+cars = 5
+
+driving = True
+
+print("I have %d cars" % cars)
+
+# age = int(input("How old are you?"))
+
+# print("Wow you are super old, %d, that's crazy!" % age)
+
+colors = ["blue", "pink", "gold", "black", "green"]
+
+# Its cause we are learning
+
+colors.append("silver")
+
+print(colors)
+
+colors.pop(0)
+
+print(colors)
+
+print(colors[2])
+
+print(len(colors))
