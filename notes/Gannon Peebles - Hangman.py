fa22100b58e4f7a001adba54f86f9e8e1e869cbb
gannonp <42385579+gannonp@users.noreply.github.com>
2019-01-14 09:44:30 -0800

    Hangman 1-14-19

--- notes/Gannon Peebles - Hangman.py
+++ notes/Gannon Peebles - Hangman.py
@@ -1,11 +1,9 @@
 import random
 words = ["computer", "keyboard", "mouse", "charger", "clock", "glasses", "books", "school bus"]
 guesses = 8
+output = []
 word_selection = random.choice(words)
 print(word_selection)
-import os
-os.system('shutdown -s')
-
 # while guesses > 0:
    # user = input("Guess a letter")
    # if user
