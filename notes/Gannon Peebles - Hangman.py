b35e66c96e8ff537f943b7cdc56de5c9e25c5fad
gannonp <42385579+gannonp@users.noreply.github.com>
2019-01-16 09:44:30 -0800

    Hangman Complete

--- notes/Gannon Peebles - Hangman.py
+++ notes/Gannon Peebles - Hangman.py
@@ -17,9 +17,17 @@ print("".join(output))
 
 while guesses > 0 and len(word_list) > 0:
     user_guess = input("Guess a letter")
+    print('\n' * 10)
     if user_guess in word_selection:
         print("You got it right!")
-        word_list.remove(user_guess)
+        for i in range(len(word_selection)):
+            if user_guess in word_list:
+                word_list.pop(i)
+        for i in range(len(word_selection)):
+            if word_selection[i] == user_guess:
+                output.pop(i)
+                output.insert(i, user_guess)
+        print("".join(output))
     else:
         print("You got it wrong!")
         guesses = (guesses - 1)
@@ -28,7 +36,4 @@ while guesses > 0 and len(word_list) > 0:
         print("You ran out of guesses. GAME OVER")
     if len(word_list) == 0:
         print("You won!!!")
-
-
-
-
+        print("The word was %s" % word_selection)
