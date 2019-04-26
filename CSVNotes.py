0f524cd1e5f596ca5aa3cf660130866cb2e43ec0
gannonp <42385579+gannonp@users.noreply.github.com>
2019-04-26 09:47:57 -0700

    CSVNotes
    April 26, 2019

--- CSVNotes.py
+++ CSVNotes.py
@@ -1,30 +1,63 @@
 import csv
-with open("Book1.csv") as old_csv:
+
+
+def divisible_by_3(num: str):
+    if not all_16_digits(num):
+        return False
+    first_num = int(num[0])
+    if first_num % 3 == 0:
+        return True
+    return False
+
+
+def divisible_by_2(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 0:
+        return True
+    return False
+
+
+def all_16_digits(num: str):
+    if len(num) == 16:
+        return True
+    else:
+        print("NOT EVERY NUMBER IS 16 DIGITS!")
+        return False
+
+
+# with open("Book1.csv") as old_csv:
+#     reader = csv.rea\(old_csv)
+#     for row in reader:
+#         old_number = int(row[0]) + 1
+#         old_number = row[0]
+#         print(old_number)
+
+# with open("Book1.csv", 'r') as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         print("Processing...")
+#
+#         for row in reader:
+#             # old_number = int(row[0]) + 1
+#             old_number = row[0]
+#             first_num = int(old_number[0])
+#             if first_num % 2 == 0:
+#                 writer.writerow(row)
+#
+# print("DONE")
+
+
+with open("Book1.csv", 'r') as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        print("Writing file...")
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
-        for row in reader:
-            old_number = int(row[0])
-            new_number = old_number + 1
-            row[0] = new_number
-            writer.writerow(row)
-            # print(int(old_number) + 1)
-            # print(old_number)
+        print("Processing...")
 
-print("OK")
-
-with open("Book1.csv") as old_csv:
-    with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        print("Writing file...")
-        reader = csv.reader(old_csv)
-        writer = csv.writer(new_csv)
         for row in reader:
-            old_number = int(row[0])  # This is a string
-            first_num = int(old_number[0])
-            if first_num % 2 == 0:
+            # old_number = int(row[0]) + 1
+            old_number = row[0]
+            if divisible_by_3(old_number) and divisible_by_2(old_number):
                 writer.writerow(row)
-            # print(int(old_number) + 1)
-            # print(old_number)
 
-print("OK")
+print("DONE")
