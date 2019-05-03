a1de8f6846969e834a4a0019430751836c28f84d
gannonp <42385579+gannonp@users.noreply.github.com>
2019-05-03 10:39:39 -0700

    Complete Validator
    May 3, 2019

--- Validator - Gannon Peebles.py
+++ Validator - Gannon Peebles.py
@@ -6,7 +6,7 @@ def sixteen_digits(number16: str):
         if len(number16) == 16:
             return True
         else:
-            print("NOT EVERY NUMBER IS 16 DIGITS!")
+            # print("NOT EVERY NUMBER IS 16 DIGITS!")
             return False
 
 
@@ -24,20 +24,21 @@ def valid_card_number(num: str):
     for number in range(len(valid_card_number_list)):
         valid_card_number_list[number] = int(valid_card_number_list[number])
     reversed_num = num[::-1]
-    reversed_num_list = list(reversed_num)
+    reversed_num_list = []
+    for i in range(len(reversed_num)):
+        reversed_num_list.append(int(reversed_num[i]))
     for index in range(len(reversed_num_list)):
         if divisible_by_2(index):
-            if reversed_num_list[index] * 2 > 9:
-                (reversed_num_list[index] * 2) - 9
+            if int(reversed_num_list[index]) * 2 > 9:
+                reversed_num_list[index] = int(reversed_num_list[index]) * 2 - 9
             else:
                 reversed_num_list[index] *= 2
     sum_numbers = sum(reversed_num_list)
     if (sum_numbers % 10) == 0:
-        print("Valid Card Number")
+        # print('\033[1m' + "                                Valid Card Number")
         return True
-    else:
-        print("Invalid Card Number")
-        return False
+
+    return False
 
 
 with open("Book1.csv", 'r') as old_csv:
@@ -47,7 +48,7 @@ with open("Book1.csv", 'r') as old_csv:
         print("Processing...")
         for row in reader:
             num = row[0]
-            if valid_card_number(row):
+            if valid_card_number(num):
                 writer.writerow(row)
         print("Done")
 
